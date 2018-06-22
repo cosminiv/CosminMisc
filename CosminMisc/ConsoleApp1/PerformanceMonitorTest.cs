865ffdc4a57f68cf2e83667436682860b49a8eb9
YRZUR\ivanc <cosmin.ivan@yr-group.ch>
2018-06-22 11:29:59 +0200

    Initial commit; performance monitor

---
+++ CosminMisc/ConsoleApp1/PerformanceMonitorTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class PerformanceMonitorTest
+    {
+        public static void RunTest()
+        {
+            PerformanceMonitor perfMon = new PerformanceMonitor();
+
+            Call_1(perfMon);
+        }
+
+        private static void Call_1(PerformanceMonitor perfMon)
+        {
+            perfMon.StartCall(Tools.GetCaller());
+            Thread.Sleep(300);
+
+            Call_1_1(perfMon);
+
+            Call_1_2(perfMon);
+
+            Thread.Sleep(400);
+
+            Call_1_1(perfMon);
+            Call_1_2(perfMon);
+
+            perfMon.EndCall();
+        }
+
+        private static void Call_1_1(PerformanceMonitor perfMon)
+        {
+            perfMon.StartCall(Tools.GetCaller());
+            Thread.Sleep(1000);
+            Call_1_1_1(perfMon);
+            perfMon.EndCall();
+        }
+
+        private static void Call_1_2(PerformanceMonitor perfMon)
+        {
+            perfMon.StartCall(Tools.GetCaller());
+            Thread.Sleep(3000);
+            perfMon.EndCall();
+        }
+
+        private static void Call_1_1_1(PerformanceMonitor perfMon)
+        {
+            perfMon.StartCall(Tools.GetCaller());
+            Thread.Sleep(100);
+            perfMon.EndCall();
+        }
+    }
+}
---
+++ CosminMisc/ConsoleApp1/Tools.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Tools
+    {
+        public static string GetCaller([CallerMemberName] string caller = null)
+        {
+            return caller;
+        }
+    }
+}
