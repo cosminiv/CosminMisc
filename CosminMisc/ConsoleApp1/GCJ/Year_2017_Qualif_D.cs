32a14c6d3340ae43261f97db9b0f7de9659cb5b1
YRZUR\ivanc <cosmin.ivan@yr-group.ch>
2018-10-23 15:50:46 +0200

    GCJ Problem D

--- CosminMisc/ConsoleApp1/GCJ/Year_2017_Qualif_D.cs
+++ CosminMisc/ConsoleApp1/GCJ/Year_2017_Qualif_D.cs
@@ -10,14 +10,21 @@ namespace ConsoleApp1.GCJ
 {
     class Year_2017_Qualif_D
     {
+        // TODO: This is way too slow - 50s to generate a solution of size 4 (max = 100)
         public string Solve(string line) {
 
-            List<Stage> stages = StageGenerator.GenerateValidStages(4);
+            Stopwatch sw = Stopwatch.StartNew();
+            List<Stage> stages = StageGenerator.GenerateValidStages(4, sw);
+
+            long t1 = sw.ElapsedMilliseconds;
             List<Stage> topStages = stages.OrderByDescending(s => s.Score).Take(5).ToList();
+            long t2 = sw.ElapsedMilliseconds;
+            Console.WriteLine($"Sort stages: {t2 - t1}ms");
 
             foreach (var stage in topStages) {
                 Console.WriteLine(stage);
             }
+
             Console.WriteLine($"{stages.Count} stages");
 
             return "";
@@ -25,30 +32,27 @@ namespace ConsoleApp1.GCJ
 
         class StageGenerator
         {
-            public static List<Stage> GenerateValidStages(int size) {
-                Stopwatch sw = Stopwatch.StartNew();
+            public static List<Stage> GenerateValidStages(int size, Stopwatch sw) {
                 List<Stage> validStages = new List<Stage>();
+                long t0 = sw.ElapsedMilliseconds;
 
+                // Generate valid rows
                 List<string> validRows = GenerateValidRows(size);
                 long t1 = sw.ElapsedMilliseconds;
-                Dictionary<int, string> validRowsMap = validRows.ToDictionary(r => r.GetHashCode());
-                long t2 = sw.ElapsedMilliseconds;
+                Console.WriteLine($"GenerateValidRows: {t1 - t0}ms ({validRows.Count} rows)");
 
-                List<List<int>> rowPermutations = GeneratePermutations(validRowsMap.Keys.ToList(), size);
+                // Generate row permutations
+                List<List<string>> rowPermutations = GeneratePermutations(validRows, size, Stage.IsValid).ToList();
                 long t3 = sw.ElapsedMilliseconds;
+                Console.WriteLine($"GeneratePermutations: {t3 - t1}ms ({rowPermutations.Count} permutations)");
 
-                foreach (List<int> rowPerm in rowPermutations) {
-                    string rowPermAsString = string.Join("\n", rowPerm.Select(rowId => validRowsMap[rowId]));
-                    Stage stage = new Stage(rowPermAsString);
-                    if (stage.IsValid())
-                        validStages.Add(stage);
+                foreach (List<string> rowPerm in rowPermutations) {
+                    Stage stage = new Stage(rowPerm);
+                    validStages.Add(stage);
                 }
                 long t4 = sw.ElapsedMilliseconds;
-
-                Console.WriteLine($"GenerateValidRows: {t1}ms ({validRows.Count} rows)");
-                Console.WriteLine($"ToDictionary: {t2 - t1}ms");
-                Console.WriteLine($"GeneratePermutations: {t3 - t2}ms ({rowPermutations.Count} permutations)");
                 Console.WriteLine($"Get valid stages: {t4 - t3}ms ({validStages.Count} stages)");
+
                 Console.WriteLine();
 
                 return validStages;
@@ -79,62 +83,73 @@ namespace ConsoleApp1.GCJ
             }
         }
 
-        public static List<List<T>> GeneratePermutations<T>(List<T> elements, int take) {
-            List<List<T>> result = new List<List<T>>();
-
+        public static IEnumerable<List<T>> GeneratePermutations<T>(List<T> elements, int take, Func<List<T>, bool> permValidator) {
             if (take == 1) {
-                result.AddRange(elements.Select(el => new List<T> { el }));
-                goto end;
+                foreach (T elem in elements) {
+                    List<T> elemAsList = new List<T> { elem };
+                    if (permValidator(elemAsList))
+                        yield return elemAsList;
+                }
+                yield break;
             }
 
-            List<List<T>> smallerPermutations = GeneratePermutations(elements, take - 1);
+            IEnumerable<List<T>> smallerPermutations = GeneratePermutations(elements, take - 1, permValidator);
 
             for (int j = 0; j <= elements.Count - 1; j++) {
-                for (int i = 0; i < smallerPermutations.Count; i++) {
-                    List<T> smallerPerm = smallerPermutations[i];
+                foreach (List<T> smallerPerm in smallerPermutations) {
                     List<T> newPerm = smallerPerm.Prepend(elements[j]).ToList();
-                    result.Add(newPerm);
+                    if (permValidator(newPerm))
+                        yield return newPerm;
                 }
             }
-
-            end:
-            return result;
         }
 
         class Stage
         {
             readonly List<string> Models;
-            
-            public Stage(int size) {
+            readonly int _Score;
+
+            //public Stage(int size) {
+            //    Models = new List<string>();
+            //    for (int i = 0; i < size; i++)
+            //        Models[i] = "";
+            //}
+
+            //public Stage(string str) {
+            //    string[] lines = str.Split('\n');
+            //    Models = lines.ToList();
+            //}
+
+            public Stage() {
                 Models = new List<string>();
-                for (int i = 0; i < size; i++)
-                    Models[i] = "";
+                _Score = 0;
             }
 
-            public Stage(string str) {
-                string[] lines = str.Split('\n');
-                Models = lines.ToList();
+            public Stage(List<string> rows) {
+                Models = rows;
+                //Models = new List<string>(rows);
+                _Score = ComputeScore();
             }
 
             public int RowCount { get { return Models.Count; } }
 
             public int ColumnCount { get { return Models[0].Length; } }
 
-            public int Score {
-                get {
-                    int sum = 0;
-                    for (int col = 0; col < Models.Count; col++) {
-                        for (int row = 0; row < Models.Count; row++) {
-                            char model = Models[row][col];
-                            if (model == '+' || model == 'x')
-                                sum++;
-                            else if (model == 'o')
-                                sum += 2;
-                        }
-                    }
+            public int Score { get { return _Score; } }
 
-                    return sum;
+            int ComputeScore() {
+                int sum = 0;
+                for (int col = 0; col < Models.Count; col++) {
+                    for (int row = 0; row < Models.Count; row++) {
+                        char model = Models[row][col];
+                        if (model == '+' || model == 'x')
+                            sum++;
+                        else if (model == 'o')
+                            sum += 2;
+                    }
                 }
+
+                return sum;
             }
 
             public string Row(int index) {
@@ -155,79 +170,79 @@ namespace ConsoleApp1.GCJ
                 return sb.ToString();
             }
 
-            internal bool IsValid() {
-                if (!AreColumnsValid()) return false;
-                if (!AreDiagonalsValid()) return false;
-                return true;
+            //internal bool IsValid() {
+            //    return AreColumnsValid(Models) && AreDiagonalsValid(Models);
+            //}
+
+            public static bool IsValid(List<string> rows) {
+                return AreColumnsValid(rows) && AreDiagonalsValid(rows);
             }
 
-            private bool AreDiagonalsValid() {
+            private static bool AreDiagonalsValid(List<string> rows) {
+                int columnCount = rows[0].Length;
+                int rowCount = rows.Count;
+
                 // top-left half, including main diagonal
-                for (int col = 1; col < Models.Count; col++) {
+                for (int i = 1; i < rowCount; i++) {
+                    int diagLength = i + 1;
                     int nonXModels = 0;
-                    for (int row = 0; row <= col; row++) {
-                        char model = Models[row][col-row];
+                    int i2 = i;
+                    int j2 = 0;
+                    for (int k = 0; k < diagLength; k++) {
+                        char model = rows[i2--][j2++];
                         if (model != '.' && model != 'x' && ++nonXModels > 1)
                             return false;
                     }
                 }
 
                 // bottom-right diagonal
-                for (int j = 1; j < Models.Count; j++) {
-                    int diagLength = Models.Count - j;
+                for (int j = 1; j < columnCount - 1; j++) {
+                    int diagLength = Math.Min(columnCount - j, rowCount);
                     int nonXModels = 0;
-
-                    int i2 = Models.Count - 1;
+                    int i2 = rowCount - 1;
                     int j2 = j;
-
                     for (int k = 0; k < diagLength; k++) {
-                        char model = Models[i2--][j2++];
+                        char model = rows[i2--][j2++];
                         if (model != '.' && model != 'x' && ++nonXModels > 1)
                             return false;
                     }
-                    
                 }
 
                 // top-right half, including main diagonal
-                for (int j = 0; j < Models.Count - 1; j++) {
-                    int diagLength = Models.Count - j;
+                for (int j = 0; j < columnCount - 1; j++) {
+                    int diagLength = Math.Min(columnCount - j, rowCount);
                     int nonXModels = 0;
-
                     int i2 = 0;
                     int j2 = j;
-
                     for (int k = 0; k < diagLength; k++) {
-                        char model = Models[i2++][j2++];
+                        char model = rows[i2++][j2++];
                         if (model != '.' && model != 'x' && ++nonXModels > 1)
                             return false;
                     }
-
                 }
 
                 // bottom-left half
-                for (int i = 1; i < Models.Count - 1; i++) {
-                    int diagLength = Models.Count - i;
+                for (int i = 1; i < rowCount - 1; i++) {
+                    int diagLength = rowCount - i;
                     int nonXModels = 0;
-
                     int i2 = i;
                     int j2 = 0;
-
                     for (int k = 0; k < diagLength; k++) {
-                        char model = Models[i2++][j2++];
+                        char model = rows[i2++][j2++];
                         if (model != '.' && model != 'x' && ++nonXModels > 1)
                             return false;
                     }
-
                 }
 
                 return true;
             }
 
-            private bool AreColumnsValid() {
-                for (int col = 0; col < Models.Count; col++) {
+            private static bool AreColumnsValid(List<string> rows) {
+                int cols = rows[0].Length;
+                for (int col = 0; col < cols; col++) {
                     int nonPlusModels = 0;
-                    for (int row = 0; row < Models.Count; row++) {
-                        char model = Models[row][col];
+                    for (int row = 0; row < rows.Count; row++) {
+                        char model = rows[row][col];
                         if (model != '.' && model != '+' && ++nonPlusModels > 1)
                             return false;
                     }
