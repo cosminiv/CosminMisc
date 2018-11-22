2e72f0dbab248f75961e12053f70003295f41401
YRZUR\ivanc <cosmin.ivan@yr-group.ch>
2018-11-22 15:17:04 +0100

    Java initial commit

---
+++ JavaMisc/JavaTestArtif/src/main/java/JavaTest/App.java
@@ -0,0 +1,12 @@
+package JavaTest;
+
+
+public final class App {
+    private App() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
