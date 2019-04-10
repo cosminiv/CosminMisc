7f807e8cc72c47a0c31c5286ce1159fa02395fea
YRZUR\ivanc <cosmin.ivan@yr-group.ch>
2019-04-10 12:15:32 +0200

    Download URLs asynchronously in parallel

--- CosminMisc/WpfApp1/MainWindow.xaml.cs
+++ CosminMisc/WpfApp1/MainWindow.xaml.cs
@@ -34,26 +34,47 @@ namespace WpfApp1
             Stopwatch sw = Stopwatch.StartNew();
 
             string[] urls = {
-                "https://news.google.com/?hl=en-US&gl=US&ceid=US:en",
+                "https://news.google.com",
                 "https://news.yahoo.com",
-                "https://www.theguardian.com/international",
+                "https://www.theguardian.com",
                 "https://www.hotnews.ro",
-                "https://www.youtube.com/",
+                "https://www.youtube.com",
                 "https://www.facebook.com",
                 "https://www.linkedin.com",
-                "https://edition.cnn.com/"
+                "https://edition.cnn.com"
             };
 
-            //Task.WhenAll()
-            //new WebClient().DownloadStringTaskAsync();
-            //await Task.Run(() => { });
-            //TaskCompletionSource
-            //Progress<ProgressReport>
+            await DownloadParallelAsync(sw, urls);
+            //await DownloadSequentiallyAsync(sw, urls);
 
-            await DownloadAsyncSequentially(sw, urls);
+            TextBox1.Text += $"{sw.Elapsed.Seconds}s\n\n";
         }
 
-        private async Task DownloadAsyncSequentially(Stopwatch sw, string[] urls) {
+        private async Task DownloadParallelAsync(Stopwatch sw, string[] urls) {
+            var tasks = urls.Select(u => Task.Run(() => DownloadUrl(u))).ToList();
+            StringBuilder sb = new StringBuilder();
+
+            while (tasks.Count > 0) {
+                string status = "";
+
+                try {
+                    Task<DownloadResult> taskDone = await Task.WhenAny(tasks);
+                    tasks.Remove(taskDone);
+                    DownloadResult dlRes = await taskDone;
+                    status = $"{dlRes.Url}: {Math.Round(dlRes.Html.Length / 1000.0)}KB";
+                }
+                catch(AggregateException) {
+                    status = "ae error";
+                }
+                catch (Exception e) {
+                    status = "error";
+                }
+
+                TextBox1.Text += $"{status}  {sw.ElapsedMilliseconds}ms \n";
+            }
+        }
+
+        private async Task DownloadSequentiallyAsync(Stopwatch sw, string[] urls) {
             string crtUrl = "";
 
             foreach (string url in urls) {
@@ -62,8 +83,8 @@ namespace WpfApp1
                 TextBox1.Text += $"{url}";
 
                 try {
-                    string html = await Task.Run(() => DownloadUrl(url));
-                    status = $"{Math.Round(html.Length / 1000.0)}KB";
+                    DownloadResult dlRes = await Task.Run(() => DownloadUrl(url));
+                    status = $"{Math.Round(dlRes.Html.Length / 1000.0)}KB";
                 }
                 catch (Exception) {
                     status = "error";
@@ -71,13 +92,15 @@ namespace WpfApp1
 
                 TextBox1.Text += $": {status}\n";
             }
-
-            TextBox1.Text += $"{sw.Elapsed.Seconds}s\n\n";
         }
 
-        string DownloadUrl(string url) {
-            using (var client = new WebClient())
-                return client.DownloadString(url);
+        DownloadResult DownloadUrl(string url) {
+            DownloadResult result = new DownloadResult { Url = url };
+            using (var client = new WebClient()) {
+                string html = client.DownloadString(url);
+                result.Html = html;
+            }
+            return result;
         }
 
         private void Button2_Click(object sender, RoutedEventArgs e) {
@@ -86,6 +109,12 @@ namespace WpfApp1
 
         private void BtnClear1_Click(object sender, RoutedEventArgs e) {
             TextBox1.Text = "";
+        }
+
+        private class DownloadResult
+        {
+            public string Url { get; set; }
+            public string Html { get; set; }
         }
     }
 }
