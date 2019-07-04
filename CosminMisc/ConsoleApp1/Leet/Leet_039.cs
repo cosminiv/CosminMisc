846659c283d297b2d939ed7bfa47bdc7302ae53e
Cosmin Ivan <ivancosmin@yahoo.com>
2019-07-04 14:33:11 +0200

    Leet 39: generate all number combinations with a certain sum

--- CosminMisc/ConsoleApp1/Leet/Leet_039.cs
+++ CosminMisc/ConsoleApp1/Leet/Leet_039.cs
@@ -14,18 +14,44 @@ namespace ConsoleApp1.Leet
     //
     class Leet_039
     {
+        int _states = 0;
+        int _smaller = 0;
+        int _bigger = 0;
         int _duplicates = 0;
 
         public void Test() {
-            var res = CombinationSum(new[] { 1, 3, 5 }, 17);
+            var res = CombinationSum(new[] { 1, 3, 4, 17 }, 23);
             foreach (var solution in res) {
                 Console.WriteLine(String.Join(" ", solution));
             }
-            Console.WriteLine($"\n{res.Count} solutions, {_duplicates} duplicates");
+            Console.WriteLine($"\n{res.Count} solutions, {_states} states, {_duplicates} duplicates, {_smaller} smaller, {_bigger} bigger");
         }
 
         public IList<IList<int>> CombinationSum(int[] candidates, int target) {
-            return Solve_Tree(candidates, target);
+            return Solve_Leet(candidates, target);
+        }
+
+        private IList<IList<int>> Solve_Leet(int[] candidates, int target) {
+            List<IList<int>> list = new List<IList<int>>();
+            Array.Sort(candidates);
+            Solve_Leet(list, new List<int>(), candidates, target, 0);
+            return list;
+        }
+
+        private void Solve_Leet(List<IList<int>> solutions, List<int> candidate, int[] nums, 
+            int remain, int start) {
+            _states++;
+            if (remain < 0)
+                return;
+            else if (remain == 0)
+                solutions.Add(new List<int>(candidate));
+            else {
+                for (int i = start; i < nums.Length && nums[i] <= remain; i++) {
+                    candidate.Add(nums[i]);
+                    Solve_Leet(solutions, candidate, nums, remain - nums[i], i); // not i + 1 because we can reuse same elements
+                    candidate.RemoveAt(candidate.Count - 1);
+                }
+            }
         }
 
         //
@@ -37,17 +63,18 @@ namespace ConsoleApp1.Leet
             long longTarget = target;
 
             Queue<int[]> factorsQueue = new Queue<int[]>();
-            // root of tree
-            factorsQueue.Enqueue(new int[candidates.Length]);
+            factorsQueue.Enqueue(new int[candidates.Length]);  // root of tree
 
             while (factorsQueue.Count > 0) {
                 int[] factors = factorsQueue.Dequeue();
                 long sum = ComputeSum(candidates, factors);
+                _states++;
 
                 if (sum == longTarget) {
-                    var solution = MakeListForResult(candidates, factors);
-                    int solutionHash = ComputeHash(solution);
+                    int solutionHash = ComputeHash(factors);
+
                     if (!solutionHashes.Contains(solutionHash)) {
+                        var solution = MakeSolution(candidates, factors);
                         solutions.Add(solution);
                         solutionHashes.Add(solutionHash);
                     }
@@ -55,6 +82,8 @@ namespace ConsoleApp1.Leet
                         _duplicates++;
                 }
                 else if (sum < longTarget) {
+                    _smaller++;
+                    //Console.WriteLine($"Looking for {longTarget - sum}");
                     // generate and enqueue all possible next states from factors array
                     for (int i = 0; i < factors.Length; i++) {
                         int[] newFactors = new int[factors.Length];
@@ -63,6 +92,9 @@ namespace ConsoleApp1.Leet
                         factorsQueue.Enqueue(newFactors);
                     }
                 }
+                else
+                    _bigger++;
+                // only add children if sum is less than target; otherwise we know we would go over
             }
 
             return solutions;
@@ -85,7 +117,7 @@ namespace ConsoleApp1.Leet
             return result;
         }
 
-        private IList<int> MakeListForResult(int[] candidates, int[] factors) {
+        private IList<int> MakeSolution(int[] candidates, int[] factors) {
             List<int> result = new List<int>(factors.Sum());
             for (int i = 0; i < candidates.Length; i++) {
                 for (int j = 0; j < factors[i]; j++) {
