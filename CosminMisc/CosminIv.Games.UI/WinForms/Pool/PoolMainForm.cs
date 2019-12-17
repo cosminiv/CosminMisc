64866d4f7ffcb545c5e72d78c9b5be432334477d
Cosmin Ivan <cosmin.ivan@fisglobal.com>
2019-12-17 17:17:44 +0100

    Pool - animation

--- CosminMisc/CosminIv.Games.UI/WinForms/Pool/PoolMainForm.cs
+++ CosminMisc/CosminIv.Games.UI/WinForms/Pool/PoolMainForm.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Timers;
 using System.Windows.Forms;
 
@@ -6,15 +7,19 @@ namespace CosminIv.Games.UI.WinForms.Pool
 {
     public partial class PoolMainForm : Form
     {
-        private int _ballX = 0;
-        private int _ballY = 100;
-        private int _ballOldX;
-        private int _ballOldY;
+        private double _ballX;
+        private double _ballY = 100;
+        private double _ballOldX;
+        private double _ballOldY;
+
+        private double _ballSpeed = 7;
+        private double _ballAngle = -1;
+
         private readonly int _ballWidth = 25;
         private readonly SolidBrush _ballBrush = new SolidBrush(Color.DarkBlue);
         private readonly SolidBrush _ballDeleteBrush = new SolidBrush(DefaultBackColor);
 
-        private readonly System.Timers.Timer _timer = new System.Timers.Timer(25);
+        private readonly System.Timers.Timer _timer = new System.Timers.Timer(15);
 
         public PoolMainForm()
         {
@@ -27,19 +32,33 @@ namespace CosminIv.Games.UI.WinForms.Pool
 
         private void TimerOnElapsed(object sender, ElapsedEventArgs e)
         {
+            UpdateBallPosition();
+
+            panel1.Invalidate();
+        }
+
+        private void UpdateBallPosition()
+        {
             _ballOldX = _ballX;
             _ballOldY = _ballY;
-            _ballX += 2;
 
-            DrawBall();
+            _ballX += _ballSpeed * Math.Cos(_ballAngle);
+            _ballY += _ballSpeed * Math.Sin(_ballAngle);
+
+            if (_ballX < 0)
+            {
+                _ballX = 0;
+                //_ballAngle = ...;
+            }
+
         }
 
         private void DrawBall()
         {
             using (Graphics graphics = panel1.CreateGraphics())
             {
-                graphics.FillEllipse(_ballDeleteBrush, _ballOldX, _ballOldY, _ballWidth, _ballWidth);
-                graphics.FillEllipse(_ballBrush, _ballX, _ballY, _ballWidth, _ballWidth);
+                graphics.FillEllipse(_ballDeleteBrush, (int)_ballOldX, (int)_ballOldY, _ballWidth, _ballWidth);
+                graphics.FillEllipse(_ballBrush, (int)_ballX, (int)_ballY, _ballWidth, _ballWidth);
             }
         }
 
