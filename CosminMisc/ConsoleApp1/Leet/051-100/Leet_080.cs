3fa1eff920df2938f6e9bc3b27f7bc136af8a505
Cosmin Ivan <cosmin.ivan@fisglobal.com>
2020-01-17 16:57:12 +0100

    Leet 80

---
+++ CosminMisc/ConsoleApp1/Leet/051-100/Leet_080.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Leet._051_100
+{
+    class Leet_080
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+
+            int readIndex = 1, writeIndex = 0;
+            int numberRead = nums[0], countRead = 1;
+            int result = 1;
+
+            while (readIndex < nums.Length)
+            {
+                if (nums[readIndex] != numberRead)
+                {
+                    numberRead = nums[readIndex];
+                    countRead = 1;
+                }
+                else
+                {
+                    countRead++;
+
+                    if (countRead > 2)
+                        writeIndex++;
+                }
+
+                readIndex++;
+            }
+
+            return result;
+        }
+    }
+}
